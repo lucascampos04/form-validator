1f890f56a2a33cf4e15a9061b67de4634619e781
Lucas Oliveira Campos <camposdlucasoli@gmail.com>
2023-06-28 18:12:03 -0300

    Conectando as rotas do formulario

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,36 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+require('../models/Categoria');
+const Categoria = mongoose.model("Categoria");
 
-router.get('/', (req, res) =>{
-    res.render('admin/index')
-})
+router.get('/admin', (req, res) => {
+    res.render('admin/index');
+});
 
 router.get('/post', (req, res) => {
-    res.send("PAGINA DE PUBLICAÇÃO")
-})
+    res.send("PAGINA DE PUBLICAÇÃO");
+});
 
 router.get('/categorias', (req, res) => {
-    res.send("PAGINA DE CATEGORIAS")
-})
+    res.render("admin/categorias");
+});
 
-module.exports = router
+router.get('/add', (req, res) => {
+    res.render("admin/addcategorias");
+});
+
+router.post('/nova', (req, res) => {
+    const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+
+    new Categoria(novaCategoria).save().then(()=>{
+        console.log("Categoria salva com sucesso")
+    }).catch((err)=> {
+        console.log(`Erro ao salvar a categoria ${err}`)
+    })
+});
+
+module.exports = router;
